2118e50f3746adc68bf8df7c78d2ce0e21420736
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-06-08 11:20:34 +0200

    fix: bad jenkins_slave_name

--- assets/wrapper.sh
+++ assets/wrapper.sh
@@ -4,4 +4,4 @@ cd /home/builds
 php -f ./sdkmanager.php
 rm -f ./agent.jar
 wget $JENKINS_URL/jnlpJars/agent.jar -O agent.jar
-java -jar ./agent.jar -jnlpUrl $JENKINS_URL/computer/apk-generator-prod01/slave-agent.jnlp -secret $JENKINS_SECRET -workDir "/home/builds"
+java -jar ./agent.jar -jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE/slave-agent.jnlp -secret $JENKINS_SECRET -workDir "/home/builds"
