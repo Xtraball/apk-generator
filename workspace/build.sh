035a02261c57a8fef299ab08f36649d19b380044
Alex-Yves CREPIN <a.crepin@xtraball.com>
2019-03-13 11:56:07 +0100

    feat: force xmx 1024m

--- workspace/build.sh
+++ workspace/build.sh
@@ -6,7 +6,7 @@ buildType=${3:-cdvBuildRelease}
 
 export JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' |cut -d '=' -f 2 |xargs)
 export ANDROID_HOME=/home/builds/android-sdk
-export GRADLE_OPTS=-Dorg.gradle.daemon=false
+export GRADLE_OPTS="-Xmx1024m -Xms1024m -XX:MaxPermSize=1024m -Dorg.gradle.daemon=false"
 
 cd "./$folder"
 
