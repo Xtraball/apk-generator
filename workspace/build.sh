5bb6475c25f29e2f7aeef5d8bc3dc9b01f842139
Anders <alex@kipsoft.fr>
2024-07-28 22:09:49 +0200

    feat: removes unneeded fixes.

--- workspace/build.sh
+++ workspace/build.sh
@@ -7,7 +7,7 @@ bundleType=${4:-bundleRelease}
 
 export JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 > /dev/null | grep 'java.home' |cut -d '=' -f 2 |xargs)
 export ANDROID_HOME=/home/builds/android-sdk
-export GRADLE_OPTS="-Xmx1024m -Xms1024m -XX:MaxPermSize=1024m -Dorg.gradle.daemon=false"
+export GRADLE_OPTS="-Xmx1024m -Xms1024m -XX:MaxMetaspaceSize=1024m -Dorg.gradle.daemon=false"
 
 cd "./$folder"
 
--- workspace/lib/Utils.php
+++ workspace/lib/Utils.php
@@ -323,66 +323,66 @@ class Utils
     public static function patchSdkApiLevel($uuid)
     {
         // Monkey patch Google API Sdk version
-        $configXmlPath = "/home/builds/{$uuid}/app/src/main/res/xml/config.xml";
-        $configXml = file_get_contents($configXmlPath);
-        $configXml = preg_replace('/<preference name="android-targetSdkVersion" value="([0-9]+)" \/>/i', '<preference name="android-targetSdkVersion" value="33" />', $configXml);
-        file_put_contents($configXmlPath, $configXml);
+//        $configXmlPath = "/home/builds/{$uuid}/app/src/main/res/xml/config.xml";
+//        $configXml = file_get_contents($configXmlPath);
+//        $configXml = preg_replace('/<preference name="android-targetSdkVersion" value="([0-9]+)" \/>/i', '<preference name="android-targetSdkVersion" value="33" />', $configXml);
+//        file_put_contents($configXmlPath, $configXml);
 
         // Open file siberian/var/apps/ionic/android/project.properties and replace target=android-XX with target=android-33
-        $projectPropertiesPath = "/home/builds/{$uuid}/project.properties";
-        $projectProperties = file_get_contents($projectPropertiesPath);
-        $projectProperties = preg_replace('/target=android-([0-9]+)/i', 'target=android-34', $projectProperties);
-        file_put_contents($projectPropertiesPath, $projectProperties);
+//        $projectPropertiesPath = "/home/builds/{$uuid}/project.properties";
+//        $projectProperties = file_get_contents($projectPropertiesPath);
+//        $projectProperties = preg_replace('/target=android-([0-9]+)/i', 'target=android-34', $projectProperties);
+//        file_put_contents($projectPropertiesPath, $projectProperties);
 
         // Same with file siberian/var/apps/ionic/android/CordovaLib/project.properties
-        $cordovaLibProjectPropertiesPath = "/home/builds/{$uuid}/CordovaLib/project.properties";
-        $cordovaLibProjectProperties = file_get_contents($cordovaLibProjectPropertiesPath);
-        $cordovaLibProjectProperties = preg_replace('/target=android-([0-9]+)/i', 'target=android-34', $cordovaLibProjectProperties);
-        file_put_contents($cordovaLibProjectPropertiesPath, $cordovaLibProjectProperties);
+//        $cordovaLibProjectPropertiesPath = "/home/builds/{$uuid}/CordovaLib/project.properties";
+//        $cordovaLibProjectProperties = file_get_contents($cordovaLibProjectPropertiesPath);
+//        $cordovaLibProjectProperties = preg_replace('/target=android-([0-9]+)/i', 'target=android-34', $cordovaLibProjectProperties);
+//        file_put_contents($cordovaLibProjectPropertiesPath, $cordovaLibProjectProperties);
 
         // Same with file siberian/var/apps/ionic/android/cdv-gradle-config.json   "SDK_VERSION": XX, with   "SDK_VERSION": 33,
-        $cdvGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
-        $cdvGradleConfig = file_get_contents($cdvGradleConfigPath);
-        $cdvGradleConfig = preg_replace('/"SDK_VERSION": ([0-9]+)/i', '"SDK_VERSION": 34', $cdvGradleConfig);
-        file_put_contents($cdvGradleConfigPath, $cdvGradleConfig);
+//        $cdvGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
+//        $cdvGradleConfig = file_get_contents($cdvGradleConfigPath);
+//        $cdvGradleConfig = preg_replace('/"SDK_VERSION": ([0-9]+)/i', '"SDK_VERSION": 34', $cdvGradleConfig);
+//        file_put_contents($cdvGradleConfigPath, $cdvGradleConfig);
 
         // Same with file siberian/var/apps/ionic/android/cdv-gradle-config.json   "SDK_VERSION": XX, with   "SDK_VERSION": 33,
-        $cdvGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
-        $cdvGradleConfig = file_get_contents($cdvGradleConfigPath);
-        $cdvGradleConfig = preg_replace('/"GRADLE_VERSION": "([0-9\.]+)"/i', '"GRADLE_VERSION": 8.1.1', $cdvGradleConfig);
-        file_put_contents($cdvGradleConfigPath, $cdvGradleConfig);
+//        $cdvGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
+//        $cdvGradleConfig = file_get_contents($cdvGradleConfigPath);
+//        $cdvGradleConfig = preg_replace('/"GRADLE_VERSION": "([0-9\.]+)"/i', '"GRADLE_VERSION": 8.1.1', $cdvGradleConfig);
+//        file_put_contents($cdvGradleConfigPath, $cdvGradleConfig);
 
         //  "AGP_VERSION": "7.0.4",
         // Same with file siberian/var/apps/ionic/android/cdv-gradle-config.json   "SDK_VERSION": XX, with   "SDK_VERSION": 33,
-        $agpGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
-        $agpGradleConfig = file_get_contents($agpGradleConfigPath);
-        $agpGradleConfig = preg_replace('/"AGP_VERSION": "([0-9\.]+)"/i', '"AGP_VERSION": 8.1.1', $agpGradleConfig);
-        file_put_contents($agpGradleConfigPath, $agpGradleConfig);
+//        $agpGradleConfigPath = "/home/builds/{$uuid}/cdv-gradle-config.json";
+//        $agpGradleConfig = file_get_contents($agpGradleConfigPath);
+//        $agpGradleConfig = preg_replace('/"AGP_VERSION": "([0-9\.]+)"/i', '"AGP_VERSION": 8.1.1', $agpGradleConfig);
+//        file_put_contents($agpGradleConfigPath, $agpGradleConfig);
 
         // app/src/main/AndroidManifest.xml
         // extract package="com.xtraball.anders.androidapp666596acb6b6e" from the file
-        $androidManifestPath = "/home/builds/{$uuid}/app/src/main/AndroidManifest.xml";
-        $androidManifest = file_get_contents($androidManifestPath);
-        // extract the value of package
-        preg_match('/package="([^"]+)"/i', $androidManifest, $matches);
-        $package = $matches[1];
+//        $androidManifestPath = "/home/builds/{$uuid}/app/src/main/AndroidManifest.xml";
+//        $androidManifest = file_get_contents($androidManifestPath);
+//        // extract the value of package
+//        preg_match('/package="([^"]+)"/i', $androidManifest, $matches);
+//        $package = $matches[1];
         // add namespace to app/build.gradle, after android {, add the following line:
         // namespace "com.xtraball.anders.androidapp666596acb6b6e"
-        $appBuildGradlePath = "/home/builds/{$uuid}/app/build.gradle";
-        $appBuildGradle = file_get_contents($appBuildGradlePath);
-        $appBuildGradle = preg_replace('/android {/i', "android {\n    namespace \"{$package}\"", $appBuildGradle);
-        file_put_contents($appBuildGradlePath, $appBuildGradle);
+//        $appBuildGradlePath = "/home/builds/{$uuid}/app/build.gradle";
+//        $appBuildGradle = file_get_contents($appBuildGradlePath);
+//        $appBuildGradle = preg_replace('/android {/i', "android {\n    namespace \"{$package}\"", $appBuildGradle);
+//        file_put_contents($appBuildGradlePath, $appBuildGradle);
 
         // remove package="org.apache.cordova" from CordovaLib/AndroidManifest.xml
-        $cordovaLibAndroidManifestPath = "/home/builds/{$uuid}/CordovaLib/AndroidManifest.xml";
-        $cordovaLibAndroidManifest = file_get_contents($cordovaLibAndroidManifestPath);
-        $cordovaLibAndroidManifest = preg_replace('/package="org.apache.cordova"/i', '', $cordovaLibAndroidManifest);
-        file_put_contents($cordovaLibAndroidManifestPath, $cordovaLibAndroidManifest);
+//        $cordovaLibAndroidManifestPath = "/home/builds/{$uuid}/CordovaLib/AndroidManifest.xml";
+//        $cordovaLibAndroidManifest = file_get_contents($cordovaLibAndroidManifestPath);
+//        $cordovaLibAndroidManifest = preg_replace('/package="org.apache.cordova"/i', '', $cordovaLibAndroidManifest);
+//        file_put_contents($cordovaLibAndroidManifestPath, $cordovaLibAndroidManifest);
 
-        $cdvBuildGradlePath = "/home/builds/{$uuid}/CordovaLib/build.gradle";
-        $cdvBuildGradle = file_get_contents($cdvBuildGradlePath);
-        $cdvBuildGradle = preg_replace('/android {/i', "android {\n    namespace \"org.apache.cordova\"", $cdvBuildGradle);
-        file_put_contents($cdvBuildGradlePath, $cdvBuildGradle);
+//        $cdvBuildGradlePath = "/home/builds/{$uuid}/CordovaLib/build.gradle";
+//        $cdvBuildGradle = file_get_contents($cdvBuildGradlePath);
+//        $cdvBuildGradle = preg_replace('/android {/i', "android {\n    namespace \"org.apache.cordova\"", $cdvBuildGradle);
+//        file_put_contents($cdvBuildGradlePath, $cdvBuildGradle);
 
         // https://services.gradle.org/distributions/gradle-8.1.1-all.zip
 //        $gradleWrapperPath = "/home/builds/{$uuid}/tools/gradle/wrapper/gradle-wrapper.properties";
@@ -407,8 +407,8 @@ class Utils
         Utils::log("CordovaLib/project.properties", "debug");
         Utils::log(file_get_contents($cordovaLibProjectPropertiesPath), "debug");
 
-        Utils::log("cdv-gradle-config.json", "debug");
-        Utils::log(file_get_contents($cdvGradleConfigPath), "debug");
+//        Utils::log("cdv-gradle-config.json", "debug");
+//        Utils::log(file_get_contents($cdvGradleConfigPath), "debug");
 
     }
 
